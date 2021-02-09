9c0107b67839ee35708e1e9e1d78cd9d51f8d8a7
Cao Mai <caomai@rocketmail.com>
2021-02-09 10:38:12 -0800

    Refactor code using split temp variable method

--- Homework/Refactoring-Code/split_temp_variable2.py
+++ Homework/Refactoring-Code/split_temp_variable2.py
@@ -2,11 +2,11 @@
 # Split temp variable
 
 def save_into_db(info):
-    print("saved into databse")
+    print(f"{info} was saved into databse")
 
 
-user_input = input('Please enter your username: ')
-save_into_db(user_input)
-user_input = int(input('Please enter your birth year: '))
-age = 2020 - user_input
-print("You are",age, "years old.")
+user_username = input('Please enter your username: ')
+save_into_db(user_username)
+user_birth_year = int(input('Please enter your birth year: '))
+age = 2020 - user_birth_year
+print("Hi", user_username, "You are",age, "years old.")
