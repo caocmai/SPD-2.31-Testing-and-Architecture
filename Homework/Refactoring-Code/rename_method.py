b85b3d321ca78f8c22f140fd20c4e737d48fcedd
Cao Mai <caomai@rocketmail.com>
2021-02-09 21:57:28 -0800

    Refactor code with more descriptive method names

--- Homework/Refactoring-Code/rename_method.py
+++ Homework/Refactoring-Code/rename_method.py
@@ -2,27 +2,27 @@
 # Rename Method
 # Reference: https://parade.com/1039985/marynliles/pick-up-lines/
 
-def cal_un_gr(graph):   # TODO: Rename this function to reflect what it's doing.
+def calc_area_under_graph(graph):   # TODO: Rename this function to reflect what it's doing.
     """Calculate the area under the input graph."""
     # bla bla bla.
     pass
 
 #####################
 
-def get_value(li):  # TODO: Rename this function to reflect what it's doing.
-    m = li[0]
-    for value in li:
-        if value > m:
-            m = value
-    return m
+def find_max_val(nums_list):  # TODO: Rename this function to reflect what it's doing.
+    max_val = nums_list[0]
+    for value in nums_list:
+        if value > max_val:
+            max_val = value
+    return max_val
 
 
 li = [5, -1, 43, 32, 87, -100]
-print(get_value(li))
+print(find_max_val(li))
 
 ############################
-def process(sentence):  # TODO: Rename this function to reflect what it's doing.
-    words = sentence[0:].split(' ')
-    return words
+def split_sentence(sentence):  # TODO: Rename this function to reflect what it's doing.
+    words_list = sentence[0:].split(' ')
+    return words_list
 
-print(process('If you were a vegetable, you’d be a ‘cute-cumber.'))
+print(split_sentence('If you were a vegetable, you’d be a ‘cute-cumber.'))
