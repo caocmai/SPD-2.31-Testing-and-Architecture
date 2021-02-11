eb9a42ce7f56fd8d32498e69768e196d3b59f526
Cao Mai <caomai@rocketmail.com>
2021-02-11 11:52:08 -0800

    Refactor code by replacing nested conditional with a gaurd clause

--- Homework/Refactoring-Code/replace_nested_conditional_with_gaurd_clauses2.py
+++ Homework/Refactoring-Code/replace_nested_conditional_with_gaurd_clauses2.py
@@ -2,12 +2,12 @@
 # Replace nested conditional with gaurd clases.
 ADJ_FACTOR = 0.7
 def get_adjusted_capital(capital, rate, duration, income):
-    result = 0
-    if (capital > 0):
-        if (rate > 0 and duration > 0):
-            result = (income / duration) * ADJ_FACTOR
-    
-    return result
+    if capital < 0:
+        return 0
 
-adjusted_capital = get_adjusted_capital(50000, 4,10, 10000)
+    if (rate > 0 and duration > 0):
+        # if (rate > 0 and duration > 0):
+        return ((income / duration) * ADJ_FACTOR)
+    
+adjusted_capital = get_adjusted_capital(50000, 4, 10, 10000)
 print(adjusted_capital)
